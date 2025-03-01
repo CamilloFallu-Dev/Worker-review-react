d9981333e0a0be336d68393fd5e29f3ef7924e52
Camillo Fallù <camillofallu9920@gmail.com>
2025-03-01 15:40:18 +0100

    fix generale applicazione

--- worker-review-react/src/App.jsx
+++ worker-review-react/src/App.jsx
@@ -1,17 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import PaginaAzienda from "./pages/PaginaAzienda";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <div className="max-w-100 mx-auto"> 
-    <Router>
+    <div className="max-w-100 mx-auto">
       <Navbar />
       <Routes>
-        <Route path="/" element={<PaginaAzienda />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/pagina-azienda" element={<PaginaAzienda />} />
       </Routes>
-    </Router>
+      <Footer />
     </div>
   );
 }
-
--- worker-review-react/src/components/Navbar.jsx
+++ worker-review-react/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logoWR from "../assets/logoWR.png"; 
+import logoWR from "../assets/logoWR.png";
 
 export default function Navbar() {
   return (
@@ -8,9 +8,17 @@ export default function Navbar() {
         <img src={logoWR} alt="Logo" className="w-10 h-auto" />
       </Link>
       <button className="text-2xl">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="30" height="30"><rect x="5" y="6" width="20" height="2"/><rect x="5" y="14" width="20" height="2"/><rect x="5" y="22" width="20" height="2"/></svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 30 30"
+          width="30"
+          height="30"
+        >
+          <rect x="5" y="6" width="20" height="2" />
+          <rect x="5" y="14" width="20" height="2" />
+          <rect x="5" y="22" width="20" height="2" />
+        </svg>
       </button>
     </nav>
   );
 }
-
