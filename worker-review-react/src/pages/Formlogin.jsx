4d16788d5e7d0938d9329fa9038eba0e9e54269c
michele <michele.vitucci99@gmail.com>
2025-03-06 17:19:57 +0100

    fix,form del login e registrazione utente

--- worker-review-react/src/pages/Formlogin.jsx
+++ worker-review-react/src/pages/Formlogin.jsx
@@ -1,27 +1,27 @@
-import React from "react";
-import{Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Formlogin() {
+  const [formData, setFormData] = useState({
+    email: localStorage.getItem("email")|| ""  ,
+    password: localStorage.getItem("password") || "" ,
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => {
+      const newData = { ...prevData, [name]: value };
+      localStorage.setItem(name, value);
+      return newData
+    });
+  };
+
   const onLogin = (event) => {
     event.preventDefault();
-
-    const form = event.target;
-    console.log({
-      name: form.email.value,
-      name: form.password.value,
-    });
+    setFormData({ email: "", password: "", });
   };
 
-  const loginWithFormData = (event) => {
-    event.preventDefault();
-
-    const formData = new FormData(event.target.form);
-    console.log({
-      email: formData.get("email"),
-      password: formData.get("password"),
-    });
-
-  };
+ 
 
   return (
     <div className="bg-green-600 py-16">
@@ -30,32 +30,36 @@ function Formlogin() {
         <h2 className="text-xl mt-2">Accedi ed inizia a recensire</h2>
       </div>
 
-      <div className="bg-white border-1 border-gray-400 rounded-md mx-4 p-6 w-72 mx-auto relative mt-12">
-        <p>Email</p>
+      <div className="bg-white border-1 border-gray-400 rounded-md mx-4 p-6 w-72 mx-auto relative mt-12 shadow-2xl">
+        <label>Email</label>
         <form onSubmit={onLogin}>
           <input
             type="email"
             name="email"
             placeholder="email"
+            value={formData.email}
+            onChange={handleChange}
             className="w-full p-2 mb-4 border border-gray-300 rounded-md"
           />
-          <p>Password</p>
+          <label>Password</label>
           <input
             type="password"
             name="password"
             placeholder="password"
+            value={formData.password}
+            onChange={handleChange}
             className="w-full p-2 mb-4 border border-gray-300 rounded-md"
           />
           <button
-            type="button"
-            onClick={loginWithFormData}
-            className="bg-green-600 w-full rounded py-2 text-white hover:bg-green-700"
+            type="submit"
+            className="bg-green-600 w-full rounded py-2 text-white hover:bg-green-700 cursor-pointer"
           >
             Accedi
           </button>
         </form>
-        <p className="mt-4 text-center text-black-600 cursor-pointer" >
-          Non sei registrato? <Link to="/Registrati">Registrati</Link></p>  
+        <p className="mt-4 text-center text-black-600 cursor-pointer">
+          Non sei registrato? <Link to="/Registrati">Registrati</Link>
+        </p>
       </div>
     </div>
   );
