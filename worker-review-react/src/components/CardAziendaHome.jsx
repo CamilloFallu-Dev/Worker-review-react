dffd00088f300022d7c706e48f3ab4db2368fe43
Camillo Fallù <camillofallu9920@gmail.com>
2025-03-06 15:45:08 +0100

    modifica homepage

--- worker-review-react/src/components/CardAziendaHome.jsx
+++ worker-review-react/src/components/CardAziendaHome.jsx
@@ -1,11 +1,15 @@
 export default function CardAziendaHome({ azienda }) {
   return (
-    <div className="flex justify-center items-start gap-10 relative shadow-xl rounded-lg bg-white m-2 p-4 transform transition duration-700 hover:scale-105 ">
-      <div className="flex justify-between gap-5">
-        <img src={azienda.imageUrl} className="w-15 h-15" />
+    <div className="flex sm:w-full mb-8 md:mb-0 p-5 shadow-md rounded-xl mr-3 ml-3 lg:w-full md:w-full bg-white transform transition duration-700 hover:scale-110">
+      <div className="w-full text-left ">
+        <img src={azienda.imageUrl} className="mb-5 h-10 w-10" />
         <div>
-          <p className="text-lg font-bold">{azienda.title}</p>
-          <p className="text-sm">{azienda.descrizione}</p>
+          <p className="text-md text-gray-800 text-left leading-normal mb-5 font-lf-normal">
+            {azienda.title}
+          </p>
+          <p className="text-md text-gray-800 text-left leading-normal mb-5 font-lf-normal">
+            {azienda.descrizione}
+          </p>
         </div>
       </div>
     </div>
--- worker-review-react/src/pages/Home.jsx
+++ worker-review-react/src/pages/Home.jsx
@@ -1,4 +1,7 @@
 import CardAziendaHome from "../components/CardAziendaHome";
+import ChiSiamo from "../components/ChiSiamo";
+import Hero from "../components/Hero";
+import Team from "../components/Team";
 
 export default function Home() {
   const aziende = [
@@ -29,48 +32,25 @@ export default function Home() {
   ];
   return (
     <div>
-      <div className="relative w-full h-64">
-        <img
-          src="./../src/assets/imageuno.jpg"
-          alt="Immagine"
-          className="w-full h-full object-cover"
-        />
-        <div className="absolute top-0 left-0 w-full h-full bg-green-800 opacity-80"></div>
-
-        <div className="absolute top-1/4 left-1/2 transform -translate-x-1/2 text-center text-white">
-          <h1 className="text-4xl font-bold transform transition duration-700 hover:scale-150">
-            Worker Review
-          </h1>
-          <h2 className="text-xl mt-2">La tua recensione con un click!</h2>
-        </div>
+      <div>
+        <Hero />
       </div>
-      <div className="bg-green-200 p-2">
-        <p className="text-lg font-bold p-2">Le aizende più recensite</p>
-        <p className="text-md pl-2 pb-10">Scopri di più...</p>
-        <div className="lg:grid lg:grid-cols-2 lg:w-220 lg:mx-auto md:grid md:grid-cols-2 md:w-180 md:mx-auto">
+      <div className="bg-green-200 pb-20 pt-20">
+        <p className="text-4xl font-bold p-2 text-center">
+          Le aizende più recensite
+        </p>
+        <p className="text-2xl pl-2 pb-10 text-center">Scopri di più...</p>
+        <div className="lg:grid lg:grid-cols-2 lg:w-220 lg:mx-auto md:grid md:grid-cols-2 md:w-180 md:mx-auto gap-5 ">
           {aziende.map((card, i) => (
             <CardAziendaHome key={i} azienda={card} />
           ))}
         </div>
       </div>
-      <div className="lg:flex lg:justify-center lg:items-center md:flex md:justify-center md:items-center ">
-        <img
-          src="./../src/assets/wrimage.png"
-          alt="Immagine"
-          className="transform transition duration-700 hover:scale-105 w-100 mx-auto"
-        />
-        <div className="mx-auto lg:w-200">
-          <p className="font-bold p-4">Chi siamo...</p>
-          <p className="p-4 text-sm">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-            porro adipisci reprehenderit dicta deserunt obcaecati minus odio,
-            quis voluptas, natus qui magni ea sit possimus alias incidunt odit
-            mollitia ex. Lorem ipsum dolor sit, amet consectetur adipisicing
-            elit. Voluptates debitis minus unde facere! Reprehenderit, autem!
-            Eligendi, explicabo molestiae debitis provident reprehenderit, saepe
-            recusandae eum numquam nobis velit magnam expedita necessitatibus.
-          </p>
-        </div>
+      <div>
+        <ChiSiamo />
+      </div>
+      <div>
+        <Team />
       </div>
     </div>
   );
